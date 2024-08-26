79053be70b14c41fc012cbe800257aed2dab829b
H82M19\Student <Student>
2024-08-26 16:04:45 +0700

    xong cau1

--- truongtuankiet.js
+++ truongtuankiet.js
@@ -126,3 +126,17 @@ const averageTotal = calcAverage4(totalsArray);
 // Log the average
 console.log('Average Total:', averageTotal.toFixed(2));
 
+console.log('filter------------');
+
+const number = [45,4,9,16,25];
+Array.prototype.myfilter = function (num){
+    let newarr = [];
+    console.log(this);
+    for(let i=0; i< this.length;i++){
+        if(this[i]>num)
+            newarr.push(this[i]);
+    }
+    return newarr;
+}
+var value = number.myfilter(10);
+console.log(value);
